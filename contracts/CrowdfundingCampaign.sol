58008528159366a0d8250908acae1294b83ff5d0
Joel Obaydin <z5362015@ad.unsw.edu.au>
2026-07-29 22:43:20 +1000

    store campaign setup vars

--- contracts/CrowdfundingCampaign.sol
+++ contracts/CrowdfundingCampaign.sol
@@ -6,6 +6,33 @@ pragma solidity ^0.8.20;
  * @notice 
  */
 contract CrowdfundingCampaign {
+    // errors
+
+    // constants
+
+    // structs
+
+    struct Milestone {
+        string description;
+        uint16 bps;
+        string proofHash;
+        bool submitted;
+        bool approved;
+        bool released;
+
+    }
+
+    // state vars
+
+    Milestone[] public milestones;
+    address public founder;
+    uint256 public goalUsd;
+    uint256 public deadline;
+    address public priceFeed;
+
+    // events
+
+
     constructor(
         address _founder,
         uint256 _goalUsd,
@@ -14,6 +41,24 @@ contract CrowdfundingCampaign {
         uint16[] memory _milestoneBps,
         address _priceFeed
     ) {
-        //logic here
+        founder = _founder;
+        goalUsd = _goalUsd;
+        deadline = _deadline;
+        priceFeed = _priceFeed;
+
+        for (uint i = 0; i < _milestoneDescriptions.length; i++) {
+            milestones.push(Milestone({
+                description: _milestoneDescriptions[i],
+                proofHash: "",
+                bps: _milestoneBps[i],
+                submitted: false,
+                approved: false,
+                released: false
+            }));
+        }
+    }
+
+    function getMilestoneCount() external view returns (uint) {
+        return milestones.length;
     }
 }
